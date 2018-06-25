4ade68f36100400ea79487a89f8234fade5bc1b7
muriithiderro <muriithiderrick56@gmail.com>
2018-06-25 16:02:01 +0300

    Create all data model for pickup points datasets

--- driver/models.py
+++ driver/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.gis.db import models as geomodels
 
-# Create your models here.
+
+
+class TestDriver(models.Model):
+    name = models.CharField(max_length=250)
+    icon = models.CharField(max_length=250)
+    description = models.CharField(max_length=250)
+    geom = geomodels.PointField(srid=4326)
+    objects=geomodels.GeoManager()
+
+class AllData(models.Model):
+    pid = models.CharField(max_length=250)
+    name = models.CharField(max_length=250)
+    geom = geomodels.PointField(srid=4326)
+    vicinity = models.CharField(max_length=255)
+    rating = models.FloatField()
+    objects=geomodels.GeoManager()
